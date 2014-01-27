435d1f4125b3b7a92d96de74a7b17002d96a6b75
Peter Brunnader <p@brunnader.at>
2014-01-27 13:06:33 +0100

    Remove old directory.

--- assigment_week_4/source/erlang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename=$(basename "$1")
-extension="${filename##*.}"
-filename="${filename%.*}"
-
-rm *.beam *.dump 2>/dev/null
-erlc *.erl
-erl -noshell -s $filename start -s init stop
