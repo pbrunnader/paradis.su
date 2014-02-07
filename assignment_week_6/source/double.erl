96ea062a303d55b438aaf1fd0d20cf33259d8c77
Peter Brunnader <peter@brunnader.at>
2014-02-07 21:30:46 +0100

    Registered process with 'name' implemented and monitor implemented as well (with an endless loop).

---
+++ assignment_week_6/source/double.erl
@@ -0,0 +1,23 @@
+% 
+% This is the "week6 assignment" for the course "Parallel and distributed programming" 
+% at the Stockholm University 
+% 
+% @author Peter Brunnader 
+% @version 1.0
+% 
+
+-module(double).
+-compile(export_all).
+
+start() ->
+	Pid = spawn(double, loop, []),
+	register(double, Pid).
+	
+loop() -> 
+	receive
+		X when is_integer(X) -> 
+			io:format("~p~n",[X*2]),
+			loop();
+		_ -> 
+			exit("Unexpected data type.")
+	end.
---
+++ assignment_week_6/source/monitor.erl
@@ -0,0 +1,28 @@
+% 
+% This is the "week6 assignment" for the course "Parallel and distributed programming" 
+% at the Stockholm University 
+% 
+% @author Peter Brunnader 
+% @version 1.0
+% 
+
+-module(monitor).
+-compile(export_all).
+
+start() ->
+	double:start(),
+	Pid = whereis(double),
+	io:format("Process 'double' with Pid ~p is (re)startet.~n",[Pid]),
+	spawn_link(monitor, loop, [Pid]).
+	
+loop(Pid) -> 
+	link(Pid),
+	process_flag(trap_exit, true),
+	io:format("Process with Pid ~p is monitored.~n",[Pid]),
+	receive 
+		{'EXIT',Pid, Why} ->
+			io:format("Process with Pid ~p chrashed because ~p.~n",[Pid,Why]),
+			monitor:start()
+	end.
+	
+	
