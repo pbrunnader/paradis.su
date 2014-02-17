5812d4d08480ded2ba64f6ba0eff879ab9bc3ecd
Peter Brunnader <peter@brunnader.at>
2014-02-18 00:38:24 +0100

    Template for the task 'gen server', unclear how to solve it.

---
+++ assignment_week_8/source/tracker.erl
@@ -0,0 +1,25 @@
+% 
+% This is the "week8 assignment" for the course "Parallel and distributed programming" 
+% at the Stockholm University 
+% 
+% @author Peter Brunnader 
+% @version 1.0
+% 
+
+-module(tracker).
+-export([start/0,i_want/2,i_am_leaving/1,who_wants/1,ping/1]).
+
+start() ->
+	done.
+
+i_want(File, IP) ->
+	done.
+
+i_am_leaving(IP) ->
+	done.
+
+who_wants(File) ->
+	done.
+
+ping(IP) ->
+	done.
