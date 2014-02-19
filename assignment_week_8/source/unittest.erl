8ad8ae8b2a72b4d8db243ed44e82b6132c7570c3
Peter Brunnader <peter@brunnader.at>
2014-02-19 01:44:42 +0100

    Added a file for the unit test.

---
+++ assignment_week_8/source/unittest.erl
@@ -0,0 +1,9 @@
+% 
+% This is the "week8 assignment" for the course "Parallel and distributed programming" 
+% at the Stockholm University 
+% 
+% @author Peter Brunnader 
+% @version 1.0
+% 
+
+-module(unittest).
