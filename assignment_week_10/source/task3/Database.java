0212e0eae86e507170630e474046fa15cc7bdc87
Peter Brunnader <peter@brunnader.at>
2014-03-11 01:49:22 +0100

    Now it is optimal.

--- assignment_week_10/source/task3/Database.java
+++ assignment_week_10/source/task3/Database.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-import java.util.Random;
-
 public class Database {
 	
 	private static long objectIdCounter = 0;
 	private static Map<Long, Integer> integerData = new HashMap<Long, Integer>();
 	private static Map<Long, Object> lockList = new HashMap<Long, Object>();
+	private static Object lock = new Object();
 
 	public static long createKey(long objectId, byte propertyId) {
 		return objectId * 256 + propertyId;
@@ -34,41 +33,22 @@ public class Database {
 
 	public static void transaction(IAction action) {
 		List<Long> accountList = action.getKeys();
-		Random random = new Random();
-		
+
 		Object lock0 = null;
 		Object lock1 = null;
 
-		if(!lockList.containsKey(accountList.get(0)))
-			lockList.put(accountList.get(0), new Object());
-
-		lock0 = lockList.get(accountList.get(0));
-
-		while(lock0 == null) { 
-			if(!lockList.containsKey(accountList.get(0)))
-				lockList.put(accountList.get(0), new Object());
-			lock0 = lockList.get(accountList.get(0));
-			try{ 
-				Thread.sleep(random.nextInt(100)); 
-			} catch (Exception e) { }
-		}
-
-		synchronized (lock0) {
-									
-			if(!lockList.containsKey(accountList.get(1)))
-				lockList.put(accountList.get(1), new Object());
-
-			lock1 = lockList.get(accountList.get(1));
-
-			while(lock1 == null) {
+		if( !lockList.containsKey(accountList.get(0)) || !lockList.containsKey(accountList.get(1)) )
+			synchronized (lock) {
+				if(!lockList.containsKey(accountList.get(0)))
+					lockList.put(accountList.get(0), new Object());
 				if(!lockList.containsKey(accountList.get(1)))
 					lockList.put(accountList.get(1), new Object());
-				lock1 = lockList.get(accountList.get(1));
-				try{ 
-					Thread.sleep(random.nextInt(100)); 
-				} catch (Exception e) { }
 			}
+		
+		lock0 = lockList.get(accountList.get(0));
+		lock1 = lockList.get(accountList.get(1));
 
+		synchronized (lock0) {
 			synchronized (lock1) {
 				action.execute();
 			}
